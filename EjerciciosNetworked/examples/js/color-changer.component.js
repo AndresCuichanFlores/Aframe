44c47a1bd53fe5162e0a6bb18d5eee42fff95a0f
AndresCuichanFlores <fa.cuichan.2016@alumnos.urjc.es>
2024-01-22 16:38:16 +0100

    Networked animacion y super hands

--- EjerciciosNetworked/examples/js/color-changer.component.js
+++ EjerciciosNetworked/examples/js/color-changer.component.js
@@ -5,6 +5,18 @@ AFRAME.registerComponent('color-changer', {
       console.log("CLICKKKKKKKKKKKKKKKKK")
       this.el.setAttribute('material', { color: this.getRandomColor() });
       NAF.utils.takeOwnership(this.el);
+
+      /*
+      this.el.setAttribute('animation', { 
+        property: 'position',
+        to: '-8 1 -12',
+        loop: 4,
+        dur: 4000,
+        easing: 'linear',
+        dir: 'alternate' 
+      });
+      */
+
     }
   },
 
